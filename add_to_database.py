42ad97ffccf20dc153ac5f1586b5e1ba5f2aa037
yasmeen16-meet <yasmeen16-meet@github.com>
2016-07-25 18:36:16 +0300

    added the databases files

---
+++ add_to_database.py
@@ -0,0 +1,13 @@
+from sqlalchemy.orm import relationship, sessionmaker
+from sqlalchemy import create_engine
+
+from database_setup import Base, Person
+
+engine = create_engine('sqlite:///toolbox.db')
+Base.metadata.create_all(engine)
+Base.metadata.bind = engine
+
+DBSessionMaker = sessionmaker(bind=engine)
+dbSession = DBSessionMaker()
+
+### Your code goes here! ###
