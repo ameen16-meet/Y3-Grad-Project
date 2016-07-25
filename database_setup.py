725831c255073f80638094775843bacb034b2d24
yasmeen16-meet <yasmeen16-meet@github.com>
2016-07-25 22:02:09 +0300

    some changes in the database

--- database_setup.py
+++ database_setup.py
@@ -8,7 +8,29 @@ class Person(Base):
     __tablename__ = 'user'
     name = Column(String)
     id = Column(Integer, primary_key=True)
-    gender = Column(String)
-    nationality = Column(String)
+    email = Column(email)
+    password = Column(password)
+    XP = Column(Integer)
+    coins = Column(Integer)
+    nationalfrom flask import Flask, render_template
+	app = Flask(__name__)
+
+#users = 
+#[
+#    {
+#       
+#  }
+#]
+
+@app.route('/')
+def index():
+  return render_template('index.html', users=users)
+
+@app.errorhandler(404)
+def page_not_found(e):
+  return render_template('404.html')
+
+if __name__ == '__main__':
+  app.run(debug=True)ity = Column(String)
     hometown = Column(String)
 
---
+++ toolbox.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+#users =
+#[
+#    {
+#	}
+#]
+
+@app.route('/')
+def index():
+  return render_template('home.html', users=users)
+
+@app.errorhandler(404)
+def page_not_found(e):
+  return render_template('404.html')
+
+if __name__ == '__main__':
+  app.run(debug=True)
